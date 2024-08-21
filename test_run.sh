42a0eb4e07789f3bd4d0b09c64d45d9d17f9d71b
Ivan Meresman Higgs <ivan.meresman@gmail.com>
2024-08-21 10:12:56 +0100

    added my own working test script

--- test_run.sh
+++ test_run.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 #$ -cwd
 #$ -j y
-#$ -pe smp 8          # cores per GPU
+#$ -pe smp 12          # request cores per GPU
 #$ -l h_rt=36:0:0    # hours runtime
 #$ -l h_vmem=11G      #  RAM per core
 #$ -l gpu=1           # request GPU
 #$ -l gpu_type=ampere
-#$ -N grafp_tc29_medeval
-#$ -o /data/home/acw723/GraFP/hpc_out
+#$ -N ivan_test_grafp_tc29_m
+#$ -o /data/home/eez083/NeuralSampleID/output/ivan_test
 #$ -m beas
 
-module load python/3.10.7
-source ../grafp_venv/bin/activate
-python test_fp.py --query_lens=1,2,3,5 --n_query_db=500 --test_snr=10 --text=1000_fma_medium_10
+# module load python/3.10.7
+# source ../grafp_venv/bin/activate
+source /data/home/eez083/.bashrc
+conda activate neural_sample_id
+# python test_fp.py --query_lens=1,2,3,5 --n_query_db=500 --test_snr=10 --test_dir=/data/EECS-Studiosync/fma_small --text=ivan_test_fma_small
+python test_fp.py --query_lens=1,2,3,5 --n_query_db=500 --test_snr=10 --test_dir=/data/home/eez083/sample_100 --text=ivan_test_sample_100
