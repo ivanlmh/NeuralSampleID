6f8a936faeefdc3dba475dd061008171bfece5f7
ivanlmh <ivan.meresman@gmail.com>
2025-01-02 13:05:43 +0000

    [update] train run scripts

--- train_run.sh
+++ train_run.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 #$ -cwd
 #$ -j y
-#$ -pe smp 16          # 16 cores per GPU
+#$ -pe smp 12          # 16 cores per GPU
 #$ -l h_rt=40:0:0    # 40 hours runtime
 #$ -l h_vmem=11G      # 11G RAM per core
-#$ -l gpu=2           # request 2 GPU
+#$ -l gpu=1           # request 2 GPU
 #$ -l gpu_type=ampere
-#$ -N grafp_tc30
-#$ -o /data/home/acw723/GraFP/hpc_out
+#$ -N ivan_1st_train_grafp
+#$ -o /data/home/eez083/NeuralSampleID/output/ivan_train
 #$ -m beas
 
-module load python/3.10.7
-source ../grafp_venv/bin/activate
+# module load python/3.10.7
+# source ../grafp_venv/bin/activate
+# python train.py --ckp=tc_30
+
+source /data/home/eez083/.bashrc
+conda activate neural_sample_id
+
 python train.py --ckp=tc_30
